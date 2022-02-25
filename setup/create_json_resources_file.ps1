0c126919ffc376e95b673c9c33251526491728be
Guillaume Plante <guillaumeplante.qc@gmail.com>
2022-02-25 10:12:53 -0500

    automatic-commit

--- setup/create_json_resources_file.ps1
+++ setup/create_json_resources_file.ps1
@@ -21,7 +21,12 @@
        Setup the module. Needs to be run only once
 #>
 
+
+
     try{
+
+
+
         If( $PSBoundParameters.ContainsKey('Path') -eq $False ){ 
             $ModulePath = (Resolve-Path "$PSScriptRoot\..").Path
             $ResPath = Join-Path $ModulePath 'res'
@@ -30,28 +35,34 @@
                 $Path = Join-Path $ResPath 'online_resources_test.json'
             }
             New-Item -Path $ResPath -ItemType Directory -ErrorAction Ignore -Force| Out-Null
-            Write-output "Generating $Path"
         }
+
+        Write-Host "===============================================================================" -f DarkRed
+        Write-Host "CREATION OF RESOURCE DEFINITION JSON FILE" -f DarkYellow;
+        Write-Host "===============================================================================" -f DarkRed    
+        Write-Host "File Path     `t" -NoNewLine -f DarkYellow ; Write-Host "$Path" -f Gray 
+        
+
 
         $InitDateStr = (Get-Date).GetDateTimeFormats()[12]
         $HostsFileResources = [System.Collections.ArrayList]::new()
         $FileUrls = [System.Collections.ArrayList]::new()
         if($Test){
-            $FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/tests/test_01.txt')
-            #$FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/tests/test_02.txt')
+            $Null=$FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/tests/test_01.txt')
+            #$Null=$FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/tests/test_02.txt')
         }else{
-            $FileUrls.Add('https://adaway.org/hosts.txt')
-            #$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Abuse-List.txt')
-            #$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/Extreme%20lists/The_Quantum_Extreme_Youtube_Ad-List.txt')
-            $FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Youtube-Ads-List.txt')
-            #$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Privacy-list.txt')
-            #$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Simply-ads-list.txt')
-            $FileUrls.Add('https://someonewhocares.org/hosts/hosts')
-            $FileUrls.Add('https://ewpratten.retrylife.ca/youtube_ad_blocklist/hosts.ipv4.txt')
-            $FileUrls.Add('https://gist.githubusercontent.com/anilshanbhag/7576643/raw/1a2b22edbfb609366548982a3638ba155bd8c33e/hosts')
-            $FileUrls.Add('https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt')
-            $FileUrls.Add('https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt')
-            $FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/blacklist/blacklisted_adservers.txt')
+            $Null=$FileUrls.Add('https://adaway.org/hosts.txt')
+            #$Null=$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Abuse-List.txt')
+            #$Null=$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/Extreme%20lists/The_Quantum_Extreme_Youtube_Ad-List.txt')
+            $Null=$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Youtube-Ads-List.txt')
+            #$Null=$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Privacy-list.txt')
+            #$Null=$FileUrls.Add('https://gitlab.com/The_Quantum_Alpha/the-quantum-ad-list/-/raw/master/Individual%20lists/The_Quantum_Simply-ads-list.txt')
+            $Null=$FileUrls.Add('https://someonewhocares.org/hosts/hosts')
+            $Null=$FileUrls.Add('https://ewpratten.retrylife.ca/youtube_ad_blocklist/hosts.ipv4.txt')
+            $Null=$FileUrls.Add('https://gist.githubusercontent.com/anilshanbhag/7576643/raw/1a2b22edbfb609366548982a3638ba155bd8c33e/hosts')
+            $Null=$FileUrls.Add('https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt')
+            $Null=$FileUrls.Add('https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt')
+            $Null=$FileUrls.Add('https://raw.githubusercontent.com/arsscriptum/PowerShell.Module.WindowsHost/master/blacklist/blacklisted_adservers.txt')
         }
 
         $InitDateStr = (Get-Date).GetDateTimeFormats()[12]
@@ -70,19 +81,19 @@
                 AddedOn     = "$InitDateStr"
             };            
             $HostsFileResources.Add($res) | Out-Null
-            Write-output "Added $name ($u)"
+            Write-Host "Added `t" -NoNewLine -f DarkYellow;  Write-Host "$name" -f Gray 
+            Write-Host "  (o) `t" -f DarkRed -NoNewLine ;  Write-Host "$u" -f Gray 
         }
 
         $JsonResources = $HostsFileResources | ConvertTo-Json
         Set-Content -Path $Path -Value $JsonResources
-        Write-output "Saved $Path"
-        $Txt = "You can now use this file to initialise the module with starting values"
 
-        Write-Host "`n`nDone! " -f DarkGreen -n ; Write-Host "$Txt" -f DarkCyan
-        Write-Host ">> `$JsonFile=`"$Path`"" -f DarkGray
-        Write-Host ">> Initialize-WinHostModule `$JsonFile`n`n"  -f DarkGray
+        Write-Host "✅ DONE You can now use this file to initialise the module with starting values"
+        Write-Host "`n  `$JsonFile=`"$Path`"" -f DarkGray
+        Write-Host "  Initialize-WinHostModule `$JsonFile`n`n" -f DarkGray
 
     }catch{
         Write-Host "ERROR " -f DarkRed -n ; Write-Host "$_" -f DarkYellow
     }
+
 
